f5fde9605ba21d408679ec24fdea31eb866554d4
Ville Keituri <ville.keituri1@gmail.com>
2021-02-28 12:25:50 +0200

    what is happening

--- Interfaces/IUIElement.cs
+++ Interfaces/IUIElement.cs
@@ -11,10 +11,7 @@ namespace TinyUI
         string FieldName { get; }
         CursorPosition ElementPosition { get;  }
         CursorPosition LabelPosition { get;  }
-<<<<<<< HEAD
-=======
         Action ElementAction { get; set; }    //TODO after project is over, change to internal set and upgrade this project to c#8.0 or beyond    
->>>>>>> 29c1da967679369a91b8bcff3d2fecea64af9a96
         CursorPosition GetCursorPosition();
         void DrawField();
     }
